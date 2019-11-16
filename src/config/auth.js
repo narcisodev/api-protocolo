b1ad690cf9377c2b310dcf9af2ceda5d684629cb
Narciso Gomes <narcisoalmeida25@gmail.com>
2019-11-16 00:30:08 -0300

    Adicionando geracao de token no login

---
+++ src/config/auth.js
@@ -0,0 +1,3 @@
+module.exports = {
+    key_secret : '3817341723183741237yuiftfsd6stf3413aaae2334211134'
+}
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,12 @@
 const Usuario = require('../models/user.model')
 const bcrypt = require('bcryptjs')
+const auth_config = require('../config/auth')
+const jwt = require('jsonwebtoken')
 
 module.exports = {
     async login(req, res) {
         const { login, pass } = req.body
-        
+
 
         const usuario = await Usuario.findOne({
             where: { login: login },
@@ -14,11 +16,17 @@ module.exports = {
         if (usuario) {
             if (await bcrypt.compare(pass, usuario.senha)) {
                 usuario.senha = undefined
-                return res.json(usuario)
+                return res.json({ usuario, token: generateToken({ id: usuario.id }) })
             }
         }
 
         return res.status(400).json({ error: 'Usuário ou senha inválidos' })
 
-    }
+    },
+}
+
+function generateToken(params = {}) {
+    return jwt.sign({ params }, auth_config.key_secret, {
+        expiresIn: 86400, //tempo de expiração dado em segundos
+    })
 }
